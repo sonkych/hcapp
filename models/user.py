1aa289f40249c8853c0719eac4e521df5b7b92fa
Aleksandr Arendarenko <aleksandr.arendarenko@gmail.com>
2023-06-02 09:11:43 +0300

    rename db tables, make some columns nullable

--- models/user.py
+++ models/user.py
@@ -6,23 +6,23 @@ Base = declarative_base()
 
 
 class Account(Base):
-    __tablename__ = "company"
+    __tablename__ = "companies"
     id = Column(Integer, primary_key=True, index=True, unique=True, autoincrement=True)
     company_name = Column(String)
     is_active = Column(Boolean, default=True)
 
 
 class User(Base):
-    __tablename__ = "user"
-    company_id = Column(Integer, ForeignKey("company.id"))
+    __tablename__ = "users"
+    company_id = Column(Integer, ForeignKey("companies.id"))
     id = Column(Integer, primary_key=True, index=True, autoincrement=True, unique=True)
     email = Column(String, unique=True)
     firstname = Column(String)
     lastname = Column(String)
     phone = Column(String)
-    telegram = Column(String)
-    department = Column(String)
-    position = Column(String)
+    telegram = Column(String, nullable=True)
+    department = Column(String, nullable=True)
+    position = Column(String, nullable=True)
     hashed_password = Column(String)
 
 
