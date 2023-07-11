1329ca3555cc002bcf4514af15e2e47b25f4ed25
Aleksandr Arendarenko <aleksandr.arendarenko@gmail.com>
2023-07-11 09:24:30 +0300

    change sign-out http method from get to post

--- auth/router.py
+++ auth/router.py
@@ -17,7 +17,7 @@ def sign_in(login_form: LoginForm):
     return crud.authenticate_user(login_form.email, login_form.password)
 
 
-@router.get("/sign-out")
+@router.post("/sign-out")
 def logout(auth_user_id=Depends(get_auth_user_id), credentials: HTTPAuthorizationCredentials = Depends(security)):
     revoke_bearer_token(credentials)
     return {"message": "Logged out successfully"}
